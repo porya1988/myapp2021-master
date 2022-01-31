868d5f558507195771b17b4037ed81fd1fccd734
porya <porya.sahraee@gmail.com>
2022-01-31 18:33:12 +0330

    delete  unneccessary tags

--- app/src/main/java/com/example/myapp2021/Registration/RView/LogView.java
+++ app/src/main/java/com/example/myapp2021/Registration/RView/LogView.java
@@ -2,13 +2,13 @@ package com.example.myapp2021.Registration.RView;
 
 public interface LogView<T> {
 
-    public void onSuccess(T responseMessage);
+    void onSuccess(T responseMessage);
 
-    public void ShowProgressbar();
+    void ShowProgressbar();
 
-    public void HideProgressbar();
+    void HideProgressbar();
 
-    public void onEmailPasswordError(T emailErrorMessage);
+    void onEmailPasswordError(T emailErrorMessage);
 
 
 }
