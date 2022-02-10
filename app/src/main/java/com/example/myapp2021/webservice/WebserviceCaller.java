19968dd03eca8c2230899b5f7363c65cc714385d
porya <porya.sahraee@gmail.com>
2022-02-10 18:55:40 +0330

    webserviceCaller edited-registration and message is ok

--- app/src/main/java/com/example/myapp2021/webservice/WebserviceCaller.java
+++ app/src/main/java/com/example/myapp2021/webservice/WebserviceCaller.java
@@ -12,6 +12,8 @@ import org.jetbrains.annotations.NotNull;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
+
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -24,39 +26,39 @@ public class WebserviceCaller {
         fservice = ApiClient.getClient().create(FService.class);
     }
 
-    public void getCategory(IMessageListner listner) {
+    public void getCategory(IMessageListner listener) {
         fservice.getCategory().enqueue(new Callback<List<Food>>() {
             @Override
             public void onResponse(@NonNull Call<List<Food>> call, @NonNull Response<List<Food>> response) {
-                listner.onSuccess(response.body());
+                listener.onSuccess(response.body());
                 Log.e("","");
              }
 
             @Override
             public void onFailure(@NotNull Call<List<Food>> call, @NotNull Throwable t) {
-                listner.onFailure("error");
+                listener.onFailure("error");
 
             }
         });
     }
 
-    public void getAllfoods(String category, IMessageListner listner) {
+    public void getAllfoods(String category, IMessageListner listener) {
         fservice.getAllfoods(category).enqueue(new Callback<List<MFoods>>() {
             @Override
             public void onResponse(@NotNull Call<List<MFoods>> call, @NotNull Response<List<MFoods>> response) {
                 List<MFoods> mFoods=response.body();
-                listner.onSuccess(mFoods);
+                listener.onSuccess(mFoods);
 
             }
 
             @Override
             public void onFailure(@NotNull Call<List<MFoods>> call, @NotNull Throwable t) {
-                listner.onFailure("Error");
+                listener.onFailure("Error");
             }
         });
     }
 
-    public void getUser(HashMap<String,Object> user,IMessageListner listner){
+    public void getUser(HashMap<String,Object> user,IMessageListner listener){
         String name= Objects.requireNonNull(user.get("name")).toString();
         String family= Objects.requireNonNull(user.get("family")).toString();
         String password= Objects.requireNonNull(user.get("password")).toString();
@@ -64,14 +66,14 @@ public class WebserviceCaller {
         Log.e("","");
         fservice.getUser(name,family,password,email).enqueue(new Callback<String>() {
             @Override
-            public void onResponse(@NonNull Call<String> call, @NonNull Response<String> response) {
-                listner.onSuccess(response.body());
+            public void onResponse(Call<String> call, Response<String> response) {
+                listener.onSuccess(response.body());
+                Log.e("","");
             }
 
             @Override
-            public void onFailure(@NonNull Call<String> call, @NonNull Throwable t) {
-                listner.onFailure("error");
-                Log.e("","");
+            public void onFailure(Call<String> call, Throwable t) {
+
             }
         });
 }
