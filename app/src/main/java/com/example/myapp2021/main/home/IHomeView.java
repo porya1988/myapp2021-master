f498053057a321b624d63380ac23ecd22240e2c2
porya <porya.sahraee@gmail.com>
2022-02-11 00:59:29 +0330

    Modifier 'public' is redundant for interface methods:debuged

--- app/src/main/java/com/example/myapp2021/main/home/IHomeView.java
+++ app/src/main/java/com/example/myapp2021/main/home/IHomeView.java
@@ -1,12 +1,8 @@
 package com.example.myapp2021.main.home;
 
-import com.example.myapp2021.model.Food;
-
-import java.util.List;
-
 public interface IHomeView<T> {
-    public void showProgressbar();
-    public void hideProgressbar();
-    public void onSuccess(T responseMessage);
-    public void onFailure(String errorResponseMessage);
+    void showProgressbar();
+    void hideProgressbar();
+    void onSuccess(T responseMessage);
+    void onFailure(String errorResponseMessage);
 }
