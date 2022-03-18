bac87dc48483c8c4bd812f63189f90e87178c576
porya1988 <porya.sahraee@gmail.com>
2022-03-18 07:01:20 +0400

    compile sdk version upgraded to 31

--- app/src/main/java/com/example/myapp2021/main/shoppinglist/AddNoteActivity.java
+++ app/src/main/java/com/example/myapp2021/main/shoppinglist/AddNoteActivity.java
@@ -1,26 +1,17 @@
 package com.example.myapp2021.main.shoppinglist;
 
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.Layout;
-import android.util.LayoutDirection;
-import android.util.Log;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.ViewCompat;
 
 import com.example.myapp2021.R;
 import com.example.myapp2021.config.AppConfiguration;
 import com.example.myapp2021.database.NAppDatabase;
 import com.example.myapp2021.databinding.ActivityAddNoteBinding;
-
 import com.example.myapp2021.model.Note;
-import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
-import com.rd.draw.data.RtlMode;
 
-import java.util.List;
+//import com.rd.draw.data.RtlMode;
 
 public class AddNoteActivity extends AppCompatActivity {
 
