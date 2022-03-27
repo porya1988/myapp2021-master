b0d471d738452cc00e4d4f242e493d777e9614af
porya1988 <porya.sahraee@gmail.com>
2022-03-27 19:13:47 +0430

    SharedPrefrences added

--- app/src/main/java/com/example/myapp2021/config/AppConfiguration.java
+++ app/src/main/java/com/example/myapp2021/config/AppConfiguration.java
@@ -3,6 +3,7 @@ package com.example.myapp2021.config;
 import android.annotation.SuppressLint;
 import android.app.Application;
 import android.content.Context;
+import android.content.SharedPreferences;
 
 import androidx.multidex.MultiDex;
 
@@ -11,6 +12,15 @@ public class AppConfiguration extends Application {
     public  AppConfiguration instance=null;
     @SuppressLint("StaticFieldLeak")
     private static Context context;
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+    public AppConfiguration(Context context){
+        sharedPreferences=context.getSharedPreferences("userInfo",Context.MODE_PRIVATE);
+        editor=sharedPreferences.edit();
+
+    }
 
 
     public static Context getContext(){return context;}
@@ -23,5 +33,9 @@ public class AppConfiguration extends Application {
         context=getApplicationContext();
     }
 
-
+    public void save(String name,String family){
+        editor.putString("name",name);
+        editor.putString("family",family);
+        editor.commit();
+    }
 }
